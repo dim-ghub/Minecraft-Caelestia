07a8d5b7717245d219245796abcd02194fc9c422
dim-ghub <mlrudasill@gmail.com>
2026-07-01 18:18:24 -0500

    Update bldit.lua to use targets table

--- bldit.lua
+++ bldit.lua
@@ -1,17 +1,20 @@
 package_name = "Minecraft-Caelestia"
 bldit_version = "1"
-global_dependencies = { "rsync", "python", "ydotool", "jq", "hyprctl", "curl", "unzip" }
+dependencies = { "rsync", "python", "ydotool", "jq", "hyprctl", "curl", "unzip" }
 
-function build()
-    -- Nothing to build for this project
-end
-
-function install(prefix)
-    -- Respect the user-defined prefix!
-    return os.execute("PREFIX=" .. prefix .. " ./scripts/install.sh")
-end
-
-function remove(prefix)
-    -- Remove using the designated prefix
-    return os.execute("PREFIX=" .. prefix .. " ./scripts/uninstall.sh")
-end
+targets = {
+    default = {
+        build = function()
+            -- Nothing to build for this project
+            return true
+        end,
+        install = function()
+            -- Respect the user-defined prefix!
+            return os.execute("PREFIX=" .. prefix .. " ./scripts/install.sh")
+        end,
+        uninstall = function()
+            -- Remove using the designated prefix
+            return os.execute("PREFIX=" .. prefix .. " ./scripts/uninstall.sh")
+        end,
+    }
+}
